0a4c4e41b13c22d837b123410ed986c9c5deb9bd
João Gabriel <joaogabriel9633@gmail.com>
2025-06-19 14:42:52 -0300

    Added dark mode

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,13 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes";
-import { Toaster } from 'sonner'
-
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
-      <>
+    <>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Toaster />
         <RouterProvider router={route} />
-      </>
+      </ThemeProvider>
+    </>
   );
 }
---
+++ pizzashop-web/src/components/theme/theme-toggle.tsx
@@ -0,0 +1,40 @@
+import { LaptopMinimal, Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "./theme-provider";
+
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="cursor-pointer">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <Sun className="size-4" />
+          Claro
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <Moon className="size-4" />
+          Escuro
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <LaptopMinimal className="size-4" />
+          Sistema
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
